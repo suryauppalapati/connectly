c8edc5fe344670b8f073d6c185e6c5ee9b4f5b42
Satya Surya Uppalapati <satyasuppalapati@gmail.com>
2026-06-30 20:56:36 +0530

    feat: add helmet middleware for enhanced security headers

--- src/app.ts
+++ src/app.ts
@@ -1,4 +1,5 @@
 import express, { type Express, type Request, type Response } from 'express';
+import helmet from 'helmet';
 import { errorHandler } from './middleware/errorHandler.ts';
 import { requestLogger } from './middleware/requestLogger.ts';
 import { v1Router } from './routes/v1/index.ts';
@@ -7,6 +8,7 @@ const app: Express = express();
 
 app.use(express.json());
 app.use(requestLogger);
+app.use(helmet());
 
 app.use('/api/v1', v1Router);
 
